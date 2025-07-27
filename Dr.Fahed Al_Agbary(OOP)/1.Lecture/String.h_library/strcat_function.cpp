14d2cd38b113f999064db0919f7bc193073d4252
ZayedSultangithup <zsmfa2005@gmail.com>
2025-07-27 15:34:29 +0300

    Create strcat_function.cpp

---
+++ Dr.Fahed Al_Agbary(OOP)/1.Lecture/String.h_library/strcat_function.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string.h>  // Needed for strcat, strlen
+using namespace std;
+int main() {
+    /*
+    //string.h functions only works with char datatype
+    const int SIZE = 10;
+    char name1[SIZE], name2[SIZE]; // Make name2 big enough to hold both strings
+    
+
+    cout << "Enter name1: ";
+    cin.get(name1, SIZE);// Mohammed Farhan
+
+    cin.ignore(1000, '\n'); // Clear input buffer
+
+    cout << "Enter name2: ";
+    cin.get(name2, SIZE);//Zayed Sultan
+    
+    //if name2 hasn't enough space program works but is unsafe
+    // Safe only if name2 has enough space
+    strcat(name2, name1);
+
+    for (int i = 0; i < strlen(name2); i++)
+        putchar(name2[i]);
+   */
+   
+    return 0;
+}
